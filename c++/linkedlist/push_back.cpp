8ca6850c297704adf91b6c043be508e1bcb761d0
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-19 22:44:20 +0530

    Update sortingAlgos subproject commit hash

---
+++ c++/linkedlist/push_back.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+public:
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = NULL;
+    tail = NULL;
+  }
+
+  void push_back(int val)
+  {
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  }
+};
+int main()
+{
+  List LL = List();
+  LL.push_back(1);
+  LL.push_back(2);
+  LL.push_back(3);
+
+  return 0;
+}
