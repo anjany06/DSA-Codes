e1499aebed273a1767e123ca3afdeb27fba14052
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-20 23:13:03 +0530

    done reverse LL

---
+++ c++/linkedlist/reverse_list.cpp
@@ -0,0 +1,88 @@
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+public:
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = NULL;
+    tail = NULL;
+  }
+
+  void push_back(int val)
+  {
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  }
+
+  void print_list()
+  {
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+      cout << temp->data << "->";
+      temp = temp->next;
+    }
+  }
+
+  void reverse_list()
+  {
+    Node *prev = NULL;
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+      Node *next = curr->next;
+
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    head = prev;
+  }
+};
+int main()
+{
+  List LL;
+  LL.push_back(1);
+  LL.push_back(2);
+  LL.push_back(3);
+  LL.push_back(4);
+  LL.push_back(5);
+
+  LL.print_list();
+  LL.reverse_list();
+  cout << endl;
+  LL.print_list();
+
+  return 0;
+}
