befc8b3702f074bca834708bd145a71ab6a82df6
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-28 23:49:39 +0530

    Conversion of Arr to DLL

---
+++ c++/linkedlist/DoublyLinkedList/arrToDLL.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *prev; // Pointer to the previous node
+public:
+  Node(int val, Node *next1, Node *prev1)
+  {
+    data = val;
+    next = next1;
+    prev = prev1;
+  }
+
+public:
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+    prev = NULL;
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = NULL;
+    tail = NULL;
+  }
+};
+
+Node *convertArrToDLL(vector<int> arr)
+{
+  Node *head = new Node(arr[0]);
+  Node *prev = head;
+  for (int i = 1; i < arr.size(); i++)
+  {
+    Node *temp = new Node(arr[i], NULL, prev);
+    prev->next = temp;
+    prev = temp;
+  }
+  return head;
+}
+void print_list(Node *head)
+{
+  Node *temp = head;
+  while (temp != NULL)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+int main()
+{
+  vector<int> arr = {1, 2, 3, 4, 5};
+  Node *head = convertArrToDLL(arr);
+  print_list(head);
+  return 0;
+}
