b81fcca202627fb873fa4ef1299894654b4320e9
Anjany Pandey <anjany.pandey06@gmail.com>
2025-04-29 19:51:36 +0530

    Brute force approach of reversing DLL ( Using stack DS)

---
+++ c++/linkedlist/DoublyLinkedList/reverse_DLL.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *prev; // Pointer to the previous node
+public:
+  Node(int val, Node *next1, Node *prev1)
+  {
+    data = val;
+    next = next1;
+    prev = prev1;
+  }
+
+public:
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+    prev = NULL;
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = NULL;
+    tail = NULL;
+  }
+};
+Node *convertArrToDLL(vector<int> arr)
+{
+  Node *head = new Node(arr[0]);
+  Node *prev = head;
+  for (int i = 1; i < arr.size(); i++)
+  {
+    Node *temp = new Node(arr[i], NULL, prev);
+    prev->next = temp;
+    prev = temp;
+  }
+  return head;
+}
+void print_list(Node *head)
+{
+  Node *temp = head;
+  while (temp != NULL)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+}
+
+// First Aproach naive brute force Tc-> o(2n) Sc-> o(n)
+Node *reverse_1(Node *head)
+{
+  stack<int> st;
+  Node *temp = head;
+  while (temp != NULL)
+  {
+    st.push(temp->data);
+    temp = temp->next;
+  }
+  temp = head;
+  while (temp != NULL)
+  {
+    temp->data = st.top();
+    st.pop();
+    temp = temp->next;
+  }
+  return head;
+}
+int main()
+{
+  vector<int> arr = {1, 2, 3, 4};
+  Node *head = convertArrToDLL(arr);
+  head = reverse_1(head);
+  print_list(head);
+  return 0;
+}
